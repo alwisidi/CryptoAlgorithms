bb393dace2247b95425405708538c4e8213b8f4f
NAWAF ALWESADIE <neafu@hotmail.com>
2021-10-23 12:58:11 +0300

    final commit

--- EnglishArabicCipher.py
+++ EnglishArabicCipher.py
@@ -1,52 +1,15 @@
-# # alphabet = [
-# #   'a', 'b', 'c', 'd', 'e', 'f',
-# #   'g', 'h', 'i', 'j', 'k', 'l',
-# #   'm', 'n', 'o', 'p', 'q', 'r',
-# #   's', 't', 'u', 'v', 'w', 'x',
-# #   'y', 'z', 'ا', 'ب', 'ت', 'ث',
-# #   'ج', 'ح', 'خ', 'د', 'ذ', 'ر',
-# #   'ز', 'س', 'ش', 'ص', 'ض', 'ط',
-# #   'ظ', 'ع', 'غ', 'ف', 'ق', 'ك',
-# #   'ل', 'م', 'ن', 'ه', 'و', 'ي'
-# # ]
+alphabet = [
+	'ا', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ',
+	'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص',
+	'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق',
+	'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي'
+]
 
-# alphabet = [
-# 	'ا', 'a', 'ب', 'b', 'ت', 'c',
-# 	'ث', 'd', 'ج', 'e', 'ح', 'f',
-# 	'خ', 'g', 'د', 'h', 'ذ', 'i',
-# 	'ر', 'j', 'ز', 'k', 'س', 'l',
-# 	'ش', 'm', 'ص', 'n', 'ض', 'o',
-# 	'ط', 'p', 'ظ', 'q', 'ع', 'r',
-# 	'غ', 's', 'ف', 't', 'ق', 'u',
-# 	'ك', 'v', 'ل', 'w', 'م', 'x',
-# 	'ن', 'y', 'ه', 'z', 'و', 'ي'
-# ]
-
-# def get_index(char):
-# 	for i in range(len(alphabet)):
-# 		x = bool(char == alphabet[i])
-# 		if(x):
-# 			print(alphabet[i])
-# 			print(x)
-# 			return i
-# 	return 54
-
-# def encrypt(src_text, key):
-# 	cipher_text = ''
-# 	for i in range(len(src_text)):
-# 		char = src_text[i].lower()
-# 		# print(char)
-# 		idx = get_index(char)
-# 		print(idx)
-# 		if(idx == 54):
-# 			cipher_text += char
-# 		else:
-# 			print(char + ": " + str((idx + key) % 54))
-# 			cipher_text += alphabet[(idx + key) % 54]
-# 	return cipher_text
-# text = "AbCD EFاب"
-# # print("Cipher text: " + encrypt(text, 2))
-# # print("Source text: " + text)
+def get_index(char):
+	for i in range(len(alphabet)):
+		if(char == alphabet[i]):
+			return i
+	return 28
 
 def encrypt(src_text, key):
 	cipher_text = ''
@@ -55,13 +18,77 @@ def encrypt(src_text, key):
 		if ord(char) >= 97 and ord(char) <= 122:
 			cipher_text += chr((ord(char) + key - 97) % 26 + 97)
 		elif ord(char) >= 1569 and ord(char) <= 1610:
-			cipher_text += "x"
 			# cipher_text += chr((ord(char) + key - 1575) % 41 + 1575)
+			idx = get_index(char)
+			if idx != 28:
+				cipher_text += alphabet[(idx + key) % 28]
+			else:
+				cipher_text += char
 		else:
 			cipher_text += char
 	return cipher_text
 
-print(ord("ا"))
-print(ord("ي"))
-for i in range(35):
-	print(chr(1575+i))
+def decrypt(cipher_text, key):
+	src_text = ''
+	for i in range(len(cipher_text)):
+		char = cipher_text[i].lower()
+		if ord(char) >= 97 and ord(char) <= 122:
+			src_text += chr((ord(char) - key - 97) % 26 + 97)
+		elif ord(char) >= 1569 and ord(char) <= 1610:
+			# cipher_text += chr((ord(char) - key - 1575) % 41 + 1575)
+			idx = get_index(char)
+			if idx != 28:
+				src_text += alphabet[(idx - key) % 28]
+			else:
+				src_text += char
+		else:
+			src_text += char
+	return src_text
+
+def break_cipher(cipher_text):
+	src_text = [''] * 54
+	for i in range(54):
+		for j in range(len(cipher_text)):
+			char = cipher_text[j].lower()
+			if ord(char) >= 97 and ord(char) <= 122:
+				src_text[i] += chr((ord(char) - (i + 1) - 97) % 26 + 97)
+			elif ord(char) >= 1569 and ord(char) <= 1610:
+				idx = get_index(char)
+				if idx != 28:
+					src_text[i] += alphabet[(idx - (i + 1)) % 28]
+				else:
+					src_text[i] += char
+			else:
+				src_text[i] += char
+		src_text[i] = str(i + 1) + " => " + src_text[i]
+	return src_text
+
+text = input('Please type your message: ')
+
+while True:
+	op = input('1. Encrypt message\n2. Decrypt message\n3. Break cipher message\nPlease choose an option: ')
+	try:
+		op = int(op)
+		if(op == 1 or op == 2 or op == 3):
+			break
+		else:
+			print('\nPlease choose a valid option!')
+	except:
+		print('\nPlease type a number!')
+
+if(op != 3):
+	key = input('Please type the \'SHIFT KEY\': ')
+	key = int(key)
+	print('\nKey: ' + str(key))
+
+if(op == 1):
+	print('Source text: ' + text)
+	print('Cipher text: ' + encrypt(text, key))
+if(op == 2):
+	print('Cipher text: ' + text)
+	print('Source text: ' + decrypt(text, key))
+
+if(op == 3):
+	list = break_cipher(text)
+	print('Cipher text: ' + text)
+	print('Source text possibilities:\n' + '\n'.join(list))
